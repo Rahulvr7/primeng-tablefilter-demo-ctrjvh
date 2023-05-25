6821864913b9f55402eacda56c18e9615b9cb6d7
Rahulvr7 <99265509+Rahulvr7@users.noreply.github.com>
2023-05-25 16:54:15 +0530

    date formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,17 @@ export class AppComponent {
   prefix:string ='=';
   loading: boolean = true;
   numericMatchModeOptions: SelectItem[];
+  d1: string = "2013-09-10T00:00:00Z";
+  timezone:string = '-07:00';
 
   activityValues: number[] = [0, 100];
 
   constructor(private customerService: CustomerService, private filterService: FilterService) {}
 
   ngOnInit() {
+    console.log(this.d1);
+    let x= new Date(this.d1)
+    console.log(x)
     this.customerService.getCustomersLarge().then(customers => {
       this.customers = customers;
       this.loading = false;
@@ -26,6 +31,7 @@ export class AppComponent {
       this.customers.forEach(
         customer => {
           if(customer.lastConnectionDate){
+            // console.log(customer.lastConnectionDate)
             (customer.lastConnectionDate = new Date(customer.lastConnectionDate))
           }
           // if(customer.runtime){
